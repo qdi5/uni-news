3e9355bc1f26d5b91a245416a6145533d86cc812
mac-wuzhe <969296206@qq.com>
2020-05-23 00:23:52 +0800

    顶部导航栏适配微信小程序

--- components/navbar/navbar.vue
+++ components/navbar/navbar.vue
@@ -1,8 +1,9 @@
 <template>
 	<view>
-		<view class="navbar">
+		<view class="navbar" :style="'padding-top:' + (statusBarHeight + navbarHeight) + 'px'">
 			<view class="navbar-fixed">
-				<view class="navbar-content">
+				<view class="placeholder" :style="'height:' + statusBarHeight + 'px'"></view>
+				<view class="navbar-content" :style="'height:' + navbarHeight + 'px;width:' + navbarWidth + 'px'">
 					<view class="navbar-search">
 						<view class="navbar-search_icon icon">
 							<uni-icons type="search" size="16" color="#999"></uni-icons>
@@ -21,9 +22,30 @@
 	export default {
 		data() {
 			return {
-				
+				statusBarHeight: 20,
+				navbarHeight: 45,
+				navbarWidth: 375
 			};
+		},
+		created () {
+			const info = uni.getSystemInfoSync()
+			// 获取状态栏的高度
+			const statusBarHeight = this.statusBarHeight = info.statusBarHeight
+			// 默认设置为窗口的宽度，避免在不支持getMenuButtonBoundingClientRect平台下，宽度不正确
+			this.navbarWidth = info.windowWidth
+			// 获取胶囊的位置
+			// #ifndef H5 | APP-PLUS | MP-ALIPAY
+			const pill = uni.getMenuButtonBoundingClientRect()
+			const left = pill.left
+			const bottom = pill.bottom
+			const top = pill.top
+			const right = pill.right
+			// 动态设置顶部搜索框的高度、宽度
+			this.navbarHeight = bottom - statusBarHeight + (top - statusBarHeight)
+			this.navbarWidth = left
+			// #endif
 		}
+		
 	}
 </script>
 
@@ -33,18 +55,17 @@
 	}
 	.navbar-fixed {
 		position: fixed;
-		display: flex;
-		align-items: center;
 		top: 0;
 		left: 0;
 		right: 0;
 		width: 100%;
-		height: 45px;
 		background: #f07373;
 	}
 	.navbar-content {
+		display: flex;
+		align-items: center;
 		width: 100%;
-		padding: 0 25px;
+		padding: 0 15px;
 		box-sizing: border-box;
 	}
 	.navbar-search {
@@ -66,4 +87,5 @@
 			color: #999;
 		}
 	}
+	
 </style>
