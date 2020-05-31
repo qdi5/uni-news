50377544dee1472d12fcdc1d3e2d77fc52485cec
mac-wuzhe <969296206@qq.com>
2020-05-31 20:08:48 +0800

    get_list云函数修改

--- cloudfunctions-aliyun/get_list/index.js
+++ cloudfunctions-aliyun/get_list/index.js
@@ -1,12 +1,23 @@
 'use strict';
 const db = uniCloud.database()
 exports.main = async (event, context) => {
+	const { name } = event
   //event为客户端上传的参数
-  const list = await db.collection('article').
+  /* const list = await db.collection('article').
 	  field({
 		  content: false
 	  }).
-	  get()
+	  get() */
+  // 聚合：更精细化的去处理数据 求和、分组，指定哪些字段
+  const list = await db.collection('article').
+	aggregate().
+	match({
+		classify: name
+	}).
+	project({
+		content: false
+	}). 
+	end()
   console.log(list)
   //返回数据给客户端
   return {
